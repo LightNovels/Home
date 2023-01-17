d8154c91c937eb88bf5a6d6ff23f7cd02dae514b
Euphilia <122472817+Euphilia@users.noreply.github.com>
2023-01-17 19:36:28 +0800

    Auto update

---
+++ README.md
@@ -0,0 +1,26 @@
+# LightNovels 轻小说
+
+> **建议使用<kbd>Ctrl</kbd> + <kbd>F</kbd>来搜索你想看的小说**
+
+<!-- Authors tables -->
+## 作者列表
+| [ハヤケン](#ハヤケン) | [鴉ぴえろ](#鴉ぴえろ) | | | |
+| :--: | :--: | :--: | :--: | :--: |
+| | | | | |
+
+
+## ハヤケン
+
+| 序号 | 书名 |
+| ---- | ---- |
+| 1 | [英雄王，為了窮盡武道而轉生～而後成為世界最強見習騎士♀～ 01.pdf](https://lightnovels.github.io/Viewer/?file=https://https://cdn.bilicdn.tk/gh/LightNovels/Home@novels/ハヤケン/ハヤケン - 英雄王，為了窮盡武道而轉生～而後成為世界最強見習騎士♀～ 01.pdf) |
+| 2 | [英雄王，為了窮盡武道而轉生～而後成為世界最強見習騎士♀～ 02.pdf](https://lightnovels.github.io/Viewer/?file=https://https://cdn.bilicdn.tk/gh/LightNovels/Home@novels/ハヤケン/ハヤケン - 英雄王，為了窮盡武道而轉生～而後成為世界最強見習騎士♀～ 02.pdf) |
+| 3 | [英雄王，為了窮盡武道而轉生～而後成為世界最強見習騎士♀～ 03.pdf](https://lightnovels.github.io/Viewer/?file=https://https://cdn.bilicdn.tk/gh/LightNovels/Home@novels/ハヤケン/ハヤケン - 英雄王，為了窮盡武道而轉生～而後成為世界最強見習騎士♀～ 03.pdf) |
+
+## 鴉ぴえろ
+
+| 序号 | 书名 |
+| ---- | ---- |
+| 1 | [轉生公主與天才千金的魔法革命 01.pdf](https://lightnovels.github.io/Viewer/?file=https://https://cdn.bilicdn.tk/gh/LightNovels/Home@novels/鴉ぴえろ/鴉ぴえろ - 轉生公主與天才千金的魔法革命 01.pdf) |
+| 2 | [轉生公主與天才千金的魔法革命 02.pdf](https://lightnovels.github.io/Viewer/?file=https://https://cdn.bilicdn.tk/gh/LightNovels/Home@novels/鴉ぴえろ/鴉ぴえろ - 轉生公主與天才千金的魔法革命 02.pdf) |
+
--- main.py
+++ main.py
@@ -1,1 +1,53 @@
-from flask import render_template
+import os
+
+template_head = '''# LightNovels 轻小说
+
+> **建议使用<kbd>Ctrl</kbd> + <kbd>F</kbd>来搜索你想看的小说**
+
+<!-- Authors tables -->
+## 作者列表
+'''
+
+template_tail = '''
+'''
+
+rendered = ''''''
+
+if __name__ == '__main__':
+    origin_dirs = os.listdir('./temp')
+    dirs = os.listdir('./temp')
+    for i in range(len(dirs)):
+        dirs[i] = f'[{dirs[i]}](#{dirs[i]})'
+    if len(dirs) > 5:
+        rendered += f'| {dirs[0]} | {dirs[1]} | {dirs[2]} | {dirs[3]} | {dirs[4]} |\n| :--: | :--: | :--: | :--: | :--: |\n'
+        for num in len(5, len(dirs), 5):
+            try:
+                tmp = str(dirs[num:num+5]).replace('[', '| ').replace(']',
+                                                                      ' |\n').replace("'", '').replace(', ', ' | ')
+                rendered += tmp
+            except IndexError:
+                tmp = str(dirs[num]).replace('[', '| ').replace(
+                    ']', ' |\n\n').replace("'", '').replace(', ', ' | ')
+                rendered += tmp
+    else:
+        authors = {}
+        tmp= str(dirs).replace('\'','')
+        tmp = str(dirs).replace('[\'', '| ').replace(
+            '\']', ' |').replace("'", '').replace(', ', ' | ')
+        tmp += ' |' * (5 - tmp.count(' |')) + '\n'
+        rendered += tmp
+        rendered += '| :--: | :--: | :--: | :--: | :--: |\n| | | | | |\n\n'
+    novels = {} 
+    for dir in origin_dirs:
+        rendered += f'\n## {dir}\n\n| 序号 | 书名 |\n| ---- | ---- |\n'
+        # rendered.replace(dir, f'[{dir}](#{dir})')
+        for root, _, fs in os.walk(f'./temp/{dir}'):
+            count = 1
+            for book in fs:
+                booklink = f'https://lightnovels.github.io/Viewer/?file=https://https://cdn.bilicdn.tk/gh/LightNovels/Home@novels/{dir}/{book}'
+                bookname = book.replace(f'{dir} - ', '')
+                rendered += f'| {count} | [{bookname}]({booklink}) |\n'
+                count += 1
+
+    with open('README.md', 'wt', encoding='utf8') as f:
+        f.write(template_head + rendered + template_tail)
