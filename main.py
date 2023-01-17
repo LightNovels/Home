f3d7e06f95cbdc05f21c0ef7cd96b262b50baf04
Euphilia <122472817+Euphilia@users.noreply.github.com>
2023-01-17 21:05:52 +0800

    update

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+from zhconv import convert
 from urllib.parse import quote
 
 template_head = '''# LightNovels 轻小说
@@ -17,10 +18,13 @@ rendered = ''''''
 if __name__ == '__main__':
     origin_dirs = os.listdir('./temp')
     dirs = os.listdir('./temp')
-    dirs.remove('.git')
-    origin_dirs.remove('.git')
+    try:
+        dirs.remove('.git')
+        origin_dirs.remove('.git')
+    except Exception:
+        pass
     for i in range(len(dirs)):
-        dirs[i] = f'[{dirs[i]}](#{dirs[i]})'
+        dirs[i] = f'[{dirs[i]}](#user-content-{dirs[i]})'
     if len(dirs) > 5:
         rendered += f'| {dirs[0]} | {dirs[1]} | {dirs[2]} | {dirs[3]} | {dirs[4]} |\n| :--: | :--: | :--: | :--: | :--: |\n'
         for num in len(5, len(dirs), 5):
@@ -46,10 +50,11 @@ if __name__ == '__main__':
         # rendered.replace(dir, f'[{dir}](#{dir})')
         for root, _, fs in os.walk(f'./temp/{dir}'):
             count = 1
+            fs.sort()
             for book in fs:
                 if book.endswith('.pdf'):
                     booklink = f'https://lightnovels.github.io/?file=https://cdn.bilicdn.tk/gh/LightNovels/Home@novels/{quote(dir)}/{quote(book)}'
-                    bookname = book.replace(f'{dir} - ', '')
+                    bookname = convert(book.replace(f'{dir} - ', ''), 'zh-cn')
                     rendered += f'| {count} | [{bookname}]({booklink}) |\n'
                     count += 1
 
